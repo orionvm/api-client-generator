9e23284aa37158ba5ff30073a771229bc407488a
Chris McClymont <chris@mcclymont.it>
2015-01-22 11:49:57 +1100

    View for python client

---
+++ lib/heroics/views/python/%{package_name}/error.py
@@ -0,0 +1,26 @@
+class APIConnectionError(Exception):
+	pass
+
+class APIError(Exception):
+	def __init__(self, json, body, status, message=None):
+		self.json = json
+		self.body = body
+		self.status = status
+
+		if message == None and json != None:
+			message = "%s: %s" % (json.get('error', ""), json.get('error_description', ''))
+		else:
+			message = body
+
+		super(APIError, self).__init__(message)
+
+class AuthenticationError(APIError):
+	pass
+class UnauthorizedError(APIError):
+	pass
+class InvalidRequestError(APIError):
+	pass
+class ValidationError(APIError):
+	def __init__(self, json, body, status):
+		msg = "%s: %s" % (json['error'], json['details'])
+		super(ValidationError, self).__init__(json, body, status, msg)
